c965aa02225f5ce7f47ecae5bf0eac309571736e
Rob-MFn-Fletcher <robroy.fletcher@gmail.com>
2015-03-29 10:29:15 -0400

    tune plots updated

--- python/makeAllTunePlots.py
+++ python/makeAllTunePlots.py
@@ -2,7 +2,7 @@
 import ROOT
 import sys
 
-
+ROOT.gROOT.SetBatch()
 
 infile = ROOT.TFile(sys.argv[1])
 outfileName = sys.argv[2]
@@ -11,13 +11,16 @@ canv = ROOT.TCanvas()
 canv.Print(outfileName+'[')
 
 for key in infile.GetListOfKeys():
-    obj = infile.Get(key)
+    print key.GetName()
+    obj = infile.Get(key.GetName())
     if obj.Class().InheritsFrom(ROOT.TDirectory.Class()):
         continue
 
     #get canvases and write 'em to PDF.
     if obj.Class().InheritsFrom(ROOT.TCanvas.Class()):
+        print "Found canvas. Writing ..."
         canv = obj
+        #canv.GetYAxis().SetRangeUser(0.5,1.0)
         canv.Print(outfileName)
 
 
